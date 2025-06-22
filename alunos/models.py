54f3084928069155c67921011614fc1ddb275ec1
gustavoalvhnr <gustavoalves.ho@gmail.com>
2025-06-22 11:19:40 -0300

    Novo commit

---
+++ alunos/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+#cerquilha para comentar
+class Estado(models.Model):
+    nome = models.CharField(max_length=50)
+    sigla = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f'{self.nome} ({self.sigla})'
+    
+class Cidade(models.Model):
+    nome = models.CharField(max_length=80)
+    estado = models.ForeignKey(Estado, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.nome} ({self.estado.sigla})'
